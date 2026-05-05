2dd9cb26c5f9fa2a4870eade8f1a7a4bebdd3aba
kirlousgish-code <kirlousgish@gmail.com>
2026-05-05 22:30:42 +0300

    ready for production

--- Data/productData.ts
+++
@@ -1,3 +0,0 @@
-// import { Product } from "@prisma/client";
-
-//   const products: Product[] = [{id: 1, name: "منتج 1", description: "وصف المنتج 1", price: 100, stock: 10, image: null, categoryId: "cat1", isActive: true, createdAt: new Date(), updatedAt: new Date()},{id: 2, name: "منتج 2", description: "وصف المنتج 2", price: 200, stock: 5, image: null, categoryId: "cat1", isActive: true, createdAt: new Date(), updatedAt: new Date()}] // جلب من الداتابيز مباشرة
